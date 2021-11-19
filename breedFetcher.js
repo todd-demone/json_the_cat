6ceb13910cc5f50f2e9cbdc4e37cee8c4028dc47
Todd Demone <tdemone1@gmail.com>
2021-11-19 17:02:43 +0000

    add test for valid cat description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,23 @@
 const request = require('request');
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
-request(
-  url,
-  (error, _response, body) => {
-    
-    if (error) {
-      console.log(`Sorry, an error occurred: ${error}`);
-      process.exit();
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, _response, body) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        const data = JSON.parse(body);
+        let description;
+        if (data.length === 0) {
+          description = `Sorry, ${breedName} could not be found.`;
+        } else {
+          description = data[0].description;
+        }
+        callback(null, description);
+      }
     }
-    
-    const data = JSON.parse(body);
-    
-    if (data.length === 0) {
-      console.log(`Sorry, ${process.argv[2]} could not be found.`);
-    } else {
-      const description = data[0].description;
-      console.log(description);
-    }
+  );
+};
 
-  }
-);
+module.exports = { fetchBreedDescription };
